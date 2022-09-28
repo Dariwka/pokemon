2cf3824656e7edd9d0ccdf2a39e0a9cae77c1264
Dariwka <77488950+Dariwka@users.noreply.github.com>
2022-09-28 15:58:12 +0300

    add new styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">Hello World!</div>;
+  return (
+    <div className="h-screen w-screen flex flex-com">
+      <div className="bg-red-200"></div>
+      <div className="bg-pink-400"></div>
+    </div>
+  );
 };
 
 export default Home;
